64e5ca64c3b058f0ff30cc4a9f871684282a576e
RikaConcept <71672285+RikaConcept@users.noreply.github.com>
2024-11-16 02:20:18 +0000

    Update products.ts

--- src/data/products.ts
+++ src/data/products.ts
@@ -6,41 +6,41 @@ export const products: Product[] = [
     description: "Kit complet Forever pour la prévention du cancer de la prostate. Une solution naturelle pour prendre soin de votre santé masculine.",
     price: 65500,
     discount: 10000,
-    image: "https://raw.githubusercontent.com/profisante/images/main/novembre-bleu.jpg"
+    image: "https://raw.githubusercontent.com/profisante/images/main/kit-prostate.jpg"
   },
   {
     title: "Forever Aloe Vera Gel",
     description: "Boisson à l'Aloe Vera pure à 99.7%, pour renforcer votre système immunitaire et favoriser une bonne digestion.",
     price: 20000,
     discount: 2000,
-    image: "https://raw.githubusercontent.com/profisante/images/main/aloe-vera-gel.jpg"
+    image: "https://raw.githubusercontent.com/profisante/public/images/aloe-vera-gel.jpg"
   },
   {
     title: "Forever Berry Nectar",
     description: "Délicieuse boisson à l'Aloe Vera enrichie en canneberge et pomme, pour une protection antioxydante optimale.",
     price: 20000,
     discount: 2000,
-    image: "https://raw.githubusercontent.com/profisante/images/main/berry-nectar.jpg"
+    image: "https://raw.githubusercontent.com/profisante/public/images/berry-nectar.jpg"
   },
   {
     title: "Forever Freedom",
     description: "Solution naturelle pour la santé de vos articulations et votre mobilité.",
     price: 29000,
     discount: 3000,
-    image: "https://raw.githubusercontent.com/profisante/images/main/freedom.jpg"
+    image: "https://raw.githubusercontent.com/profisante/public/images/freedom.jpg"
   },
   {
     title: "Forever Arctic Sea",
     description: "Complément alimentaire riche en oméga-3 (EPA et DHA). Idéal pour la santé du cœur, du cerveau, de la vision et des articulations. Combat les effets du vieillissement.",
     price: 24000,
     discount: 2000,
-    image: "https://raw.githubusercontent.com/profisante/images/main/arctic-sea-promo.jpg"
+    image: "https://raw.githubusercontent.com/profisante/public/images/arctic-sea-promo.jpg"
   },
   {
     title: "Forever CardioHealth",
     description: "Formule avancée pour maintenir une bonne santé cardiaque.",
     price: 26500,
     discount: 1500,
-    image: "https://raw.githubusercontent.com/profisante/images/main/cardio-health.jpg"
+    image: "https://raw.githubusercontent.com/profisante/public/images/cardio-health.jpg"
   }
 ];
